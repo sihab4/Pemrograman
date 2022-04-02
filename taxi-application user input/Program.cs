ab9640255f54443f239cef4f233bfeb44b83af38
sihab4 <rayzshyhab@gmail.com>
2022-04-02 09:20:44 +0700

    remake user input

---
+++ taxi-application user input/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace membuatTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object dari class Taxi
+            Taxi taxi = new Taxi();
+       
+
+            Console.Write("Nama driver : ");
+            taxi.DriverName = Console.ReadLine();
+
+            Console.Write("On Duty? [YES or NO] : ");
+            taxi.OnDuty = Console.ReadLine().ToLower() == "yes" ? true : false;
+
+            if (taxi.OnDuty) 
+            {
+                Console.Write("Number of Passenger : ");
+                taxi.NumPassenger = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+                taxi.TaxiInfo();
+                taxi.PickUpPassenger();
+                taxi.DropOffPassenger();
+
+            }
+            else
+            {
+                Console.WriteLine();
+                taxi.PickUpPassenger();
+                taxi.DropOffPassenger();
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi-application user input/taxi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace membuatTaxi
+{
+    public class Taxi
+
+    {   //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            if (OnDuty == true)
+            {
+                Console.WriteLine("Onduty = Yes");
+                Console.WriteLine("Number of Passenger {0}", NumPassenger);
+            }
+            else
+            {
+             
+                Console.WriteLine("{0} tidak sedang menjemput penumpang", DriverName);
+            }
+            
+        }
+        public void DropOffPassenger()
+        {
+            if(NumPassenger != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} sedang menngantar penumpang", DriverName);
+            }
+        }
+    }
+}
